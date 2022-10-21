5c55c7679ac72892f2e57924b27cc174dafe5280
RaulCano <91997608+calaveiraUwU@users.noreply.github.com>
2022-10-22 01:31:08 +0200

    Update FRingParams.h

--- FRingParams.h
+++ FRingParams.h
@@ -8,24 +8,16 @@ struct FRingParams
 {
 	GENERATED_BODY()
 
-	/**
-	Number of asteroids in the ring
-	*/
-
+	
+	//Number of asteroids in the ring
 	UPROPERTY(EditAnywhere, BlueprintReadonly)
 	int NumAsteroids = 100;
-	/**
-	Total radius of the ring
-	*/
+	
+	//Total radius of the ring
 	UPROPERTY(EditAnywhere, BlueprintReadonly)
 	float MajorRadius = 1000.f;
-	/**
-	Width of the ring
-	*/
+
+	//Width of the ring
 	UPROPERTY(EditAnywhere, BlueprintReadonly)
 	float MinorRadius = 10000.f;
-	/**
-	Rotation of the ring
-	*/
-
 };
