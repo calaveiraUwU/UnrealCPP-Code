e63661348cff6770dbd04d4ce033db34795fdba4
RaulCano <91997608+calaveiraUwU@users.noreply.github.com>
2022-10-22 02:29:18 +0200

    Update FalconMilenial.cpp

--- FalconMilenial.cpp
+++ FalconMilenial.cpp
@@ -16,38 +16,46 @@ AFalconMilenial::AFalconMilenial()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	//Generamos el objeto padre, le ponemos un nombre para el editor y lo asignamos como tal.
 	rootComponentC = CreateDefaultSubobject<USceneComponent>(TEXT("rootComponentC"));
 	SetRootComponent(rootComponentC);
+	
+	//Generamos el objeto nave, le ponemos un nombre para el editor y lo asignamos debajo de el objeto padre.
 	StaticMeshC = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Ship"));
 	StaticMeshC->SetupAttachment(rootComponentC);
-	//StaticMeshC->SetCollisionResponseToChannel()
+	
+	//Generamos el objeto cañon, le ponemos un nombre para el editor y lo asignamos debajo de la nave.
 	Canion = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Canion"));
 	Canion->SetupAttachment(StaticMeshC);
 
+	//Generamos el objeto que sujeta la cámara y le da un movimiento, le ponemos un nombre para el editor y lo asignamos debajo del componente padre.
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraArm"));
 	CameraBoom->SetupAttachment(rootComponentC);
-	CameraBoom->TargetArmLength = 700.0f;
-	CameraBoom->SetRelativeRotation(FRotator(-15, 0, 0));
-	CameraBoom->bEnableCameraLag = true;
-	CameraBoom->bEnableCameraRotationLag = true;
-	CameraBoom->CameraLagSpeed = 10.f;
+	CameraBoom->TargetArmLength = 700.0f;//Longitud del brazo
+	CameraBoom->SetRelativeRotation(FRotator(-15, 0, 0));//Rotación del brazo.
+	CameraBoom->bEnableCameraLag = true;//Valor verdadero para que tenga movimiento el brazo.
+	CameraBoom->bEnableCameraRotationLag = true;//Valor verdadero para que tenga rotación el brazo.
+	CameraBoom->CameraLagSpeed = 10.f;//Cuanto movimiento va a tener.
 
-
+	//Generamos el objeto que sirve de referencia al laser, le ponemos un nombre para el editor y lo asignamos debajo del componente cañon.
 	spawnPoint1 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("SP1"));
 	spawnPoint1->SetupAttachment(Canion);
 
+	//Generamos el objeto que sirve de referencia al laser, le ponemos un nombre para el editor y lo asignamos debajo del componente cañon.
 	spawnPoint2 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("SP2"));
 	spawnPoint2->SetupAttachment(Canion);
 
+	//Generamos el objeto que hace de cámara, le ponemos un nombre para el editor y lo asignamos debajo del componente brazo.
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
 	FollowCamera->SetupAttachment(CameraBoom);
 
+	//Generamos el objeto que hace de caja de colisones, le ponemos un nombre para el editor y lo asignamos debajo del componente padre.
 	TriggerBox = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxCollider"));
-	TriggerBox->SetBoxExtent(StaticMeshC->GetRelativeScale3D(), true);
-	TriggerBox->SetCollisionProfileName(TEXT("Trigger"));
+	TriggerBox->SetBoxExtent(StaticMeshC->GetRelativeScale3D(), true);//Lo escalamos segun los valores de la nave.
+	TriggerBox->SetCollisionProfileName(TEXT("Trigger"));//Le ponemos un nombre al sistema de colisiones.
 	TriggerBox->SetupAttachment(rootComponentC);
-	//FVector(13.02f, 9.50f, 1.85f)
 
+	//Generamos dos eventos dínamicos en relación de la caja de colisones para saber cuando se han activado o desactivado respectivamente.
 	TriggerBox->OnComponentBeginOverlap.AddDynamic(this, &AFalconMilenial::OnOverlapBegin);
 	TriggerBox->OnComponentEndOverlap.AddDynamic(this, &AFalconMilenial::OnOverlapEnd);
 }
@@ -70,24 +78,30 @@ void AFalconMilenial::Tick(float DeltaTime)
 
 void AFalconMilenial::Pitch(float value)
 {
+	//Le añadimos a la rotación X local una rotación que dependerá del input que le demos.
 	AddActorLocalRotation(FRotator(value * PitchSpeed * GetWorld()->DeltaTimeSeconds, 0.f, 0.f));
 }
 
 void AFalconMilenial::Yaw(float value)
 {
+	//Le añadimos a la rotación Y local una rotación que dependerá del input que le demos.
 	AddActorLocalRotation(FRotator(0.f, value * PitchSpeed * GetWorld()->DeltaTimeSeconds, 0.f));
 }
 
 void AFalconMilenial::Roll(float value)
 {
+	//Le añadimos a la rotación Z local una rotación que dependerá del input que le demos.
 	AddActorLocalRotation(FRotator(0.f, 0.f, value * RollSpeed * GetWorld()->DeltaTimeSeconds));
 }
 
 void AFalconMilenial::Forward(float value)
 {
+	//Le asignamos un valor al float que almacena la velocidad del objeto.
 	CurrentSpeed = FMath::FInterpConstantTo(CurrentSpeed, Speed * value, GetWorld()->DeltaTimeSeconds, ForwardAcceleration);
+	//Le añadimos este valor a la posición X de el objeto.
 	AddActorLocalOffset(FVector(CurrentSpeed* GetWorld()->DeltaTimeSeconds, 0.f, 0.f));
 
+	//Modificamos la intensidad del material dínamico de la nave con el valor axis de la función.
 	ChangePower(value);
 }
 
@@ -101,6 +115,8 @@ void AFalconMilenial::RotateX(float value)
 void AFalconMilenial::RotateY(float value)
 {
 	UE_LOG(LogTemp, Warning, TEXT("Num=%f"), CameraBoom->GetRelativeRotation().Pitch);
+	
+	//Comprobamos que el valor no supere los límites establecidos y rotamos el brazo de la cámara en su eje X.
 	if (CameraBoom->GetRelativeRotation().Pitch + (value * RollSpeed * GetWorld()->DeltaTimeSeconds) >= _minRotation && CameraBoom->GetRelativeRotation().Pitch + (value * RollSpeed * GetWorld()->DeltaTimeSeconds) <= _maxRotation)
 		CameraBoom->AddRelativeRotation(FRotator(value * RollSpeed * GetWorld()->DeltaTimeSeconds, 0.f, 0.f));
 }
@@ -109,11 +125,16 @@ void AFalconMilenial::ShootPressed()
 {
 
 	UWorld* World = GetWorld();
+		//Si el valor del cañon es verdadero dispara por el primero si no dispara por el segundo.
 		if (canionPoint)
 		{
+			//Obtenemos la posición del punto de referencia.
 			const FTransform SpawnLocAndRotation = spawnPoint1->GetComponentTransform();
+			//Generamos una instancia de tipo retrasada para poder pasarle valores y cuando lo tenga todo poder acabar la instancia pasandole la clase.
 			ALaser* Laser = World->SpawnActorDeferred<ALaser>(ALaser::StaticClass(), SpawnLocAndRotation);
+			//Le pasamos la velocidad que queremos más la velocidad actual de la nave para que siempre salga a una velocidad moderada y que no se quede atrás.
 			Laser->SetLaserVariables(10000.f + CurrentSpeed);
+			//Finalmente instanciamos el objeto dínamico.
 			Laser->FinishSpawning(SpawnLocAndRotation);
 		}
 		else
@@ -123,16 +144,20 @@ void AFalconMilenial::ShootPressed()
 			Laser->SetLaserVariables(10000.f + CurrentSpeed);
 			Laser->FinishSpawning(SpawnLocAndRotation);
 		}
+	
+		//Invertimos el valor actual del punto de disparo para que sea el contrario.
 		canionPoint = !canionPoint;
 }
 
 void AFalconMilenial::Shoot()
 {
+	//Mientrás se presiona el botón iniciamos el reloj y llamamos a la función que dispara cuando se cumpla el delay.
 	GetWorldTimerManager().SetTimer(TimerHandler, this, &AFalconMilenial::ShootPressed, 0.4f, true, 0.f);
 }
 
 void AFalconMilenial::ShootUnpressed()
 {
+	//Limpiamos el reloj.
 	GetWorldTimerManager().ClearTimer(TimerHandler);
 }
 
@@ -146,6 +171,7 @@ void AFalconMilenial::ShootUnpressed()
 
 void AFalconMilenial::OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	//Cuando sea otro objeto que no sea la nave nos muestra el número de asteroide que representa del HISM.
 	if (OtherActor && (OtherActor != this) && OtherComp)
 	{
 		FString IntAsString = FString::FromInt(SweepResult.Item);
@@ -155,6 +181,7 @@ void AFalconMilenial::OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, 
 
 void AFalconMilenial::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
 {
+	//Cuando salimos de otro objeto que no sea la nave nos muestra que hemos salido de la colisión.
 	if (OtherActor && (OtherActor != this) && OtherComp)
 	{
 		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Overlap End"));
@@ -165,7 +192,7 @@ void AFalconMilenial::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* 
 void AFalconMilenial::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
+	//Gestionamos los imputs y las llamadas de funciones correspondientes.
 	PlayerInputComponent->BindAxis("UpDown", this, &AFalconMilenial::Pitch);
 	PlayerInputComponent->BindAxis("RigthLeft", this, &AFalconMilenial::Yaw);
 	PlayerInputComponent->BindAxis("Forward", this, &AFalconMilenial::Forward);
