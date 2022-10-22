bbc633048be7c933f04cb826012823eb2e6534c3
RaulCano <91997608+calaveiraUwU@users.noreply.github.com>
2022-10-22 02:41:18 +0200

    Update Laser.cpp

--- Laser.cpp
+++ Laser.cpp
@@ -11,14 +11,26 @@ ALaser::ALaser()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	
+	//Establecemos el objeto y le establecemos que sea el componente padre.
 	canionMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CanionMesh"));
 	SetRootComponent(canionMesh);
+	
+	//Buscamos el Material y lo guardamos en una variable.
 	static ConstructorHelpers::FObjectFinder<UMaterial> LaserMat(TEXT("/Game/Materials/LaserMaterial"));
+	
+	//Buscamos el Objeto y lo guardamos en una variable.
 	static ConstructorHelpers::FObjectFinder<UStaticMesh> LaserMesh(TEXT("/Game/StarterContent/Shapes/Shape_Cube.Shape_Cube"));
+	
+	//Comprobamos que el objeto existe y no esta vacio y se lo establecemos a el láser.
 	if (LaserMesh.Succeeded())
 		canionMesh->SetStaticMesh(LaserMesh.Object);
+	
+	//Comprobamos que el material existe y no esta vacio y se lo establecemos a el láser.
 	if (LaserMat.Succeeded())
 		canionMesh->SetMaterial(0, LaserMat.Object);
+	
+	//Le establecemos la escala para que se vea alargado y visible en el mapa.
 	canionMesh->SetWorldScale3D(FVector(1.f, 0.04f, 0.04f));
 }
 
@@ -31,6 +43,7 @@ void ALaser::BeginPlay()
 
 void ALaser::SetLaserVariables(float speedVal)
 {
+	//Le establecemos la velocidad al objeto que le pasamos por valor al instanciarlo.
 	speed = speedVal;
 }
 
@@ -39,6 +52,8 @@ void ALaser::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	AddActorLocalOffset(FVector(speed* DeltaTime, 0.f, 0.f));
+	//Movemos el objeto cuando la velocidad sea mayor que 0.
+	if(speed>0)
+		AddActorLocalOffset(FVector(speed* DeltaTime, 0.f, 0.f));
 }
 
